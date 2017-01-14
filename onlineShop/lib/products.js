05f56eb95f37ac2f8bb4f1191515bd464546a711
wendylop <wendy.alq@gmail.com>
2017-01-14 13:39:35 +0100

    list of products

---
+++ onlineShop/lib/products.js
@@ -0,0 +1,32 @@
+Products = [
+    {
+        title: 'The Big Bang Theory',
+        price: 12345,
+        image: 'img/Serie1.jpg'
+    },
+    {
+        title: 'Game of Thrones',
+        price: 12345,
+        image: 'img/Serie2.jpg'
+    },
+    {
+        title: 'Greys Anatomy',
+        price: 12345,
+        image: 'img/Serie3.jpg'
+    },
+    {
+        title: 'The Walking Dead',
+        price: 12345,
+        image: 'img/Serier4.jpg'
+    },
+    {
+        title: 'Breaking Bad',
+        price: 12345,
+        image: 'img/Serie5.jpg'
+    },
+    {
+        title: 'Downton Abbey',
+        price: 12345,
+        image: 'img/Serie6.jpg'
+    },
+];
